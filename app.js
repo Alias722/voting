a1e60417877ff3baedf3a2b70c908383b8a8c220
alias722 <maxchengboy@gmail.com>
2021-03-05 19:44:58 +0800

    viewip

--- app.js
+++ app.js
@@ -1,15 +1,30 @@
 const express = require("express")
-const bodyParser = require("body-parser")
-
 const app = express()
+
+const bodyParser = require("body-parser")
+const jsonParser = bodyParser.json()//json parser                                                                                                           
+const urlencodedParser = bodyParser.urlencoded({extended: false})//form parser  
 const port = 17002
 
+app.all('/',(req,res)=>{
+	var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress
+	console.log(Date())
+	console.log("IP from : %d",ip)
+})
 
-app.get("/",(req,res)=>{
-	res.send("Testing")
+app.get('/',(req,res)=>{
+	res.send("hello world")
+})
+app.post('/auth',urlencodedParser,(req,res) => {
+	if(!req.body) return res.sendStatus(404)        
+	var tosend = "POST request here\nYour name is "+req.body.name+", and your age is "+req.body.age+".\n Is it correct?"
+	console.log("Inside post auth")
+	console.log(req.body.name)
+	console.log(req.body.age)
+	res.send(tosend.toString)
 })
 
 app.listen(port,()=>{
-	console.log("Opening voting system")
-	console.log("Running on %d port",port)
+	console.log("the voting system is running on %d port",port)
+
 })
