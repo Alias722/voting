9f892f433505511dc48b85177a220ff4e88b8b38
alias722 <maxchengboy@gmail.com>
2021-03-06 11:30:49 +0800

    adduser_ok

--- app.js
+++ app.js
@@ -23,11 +23,15 @@ conn.connect((err)=>{
 	if(err) throw err;
 	console.log("connection success!!")
 })
+/*conn.connect((err)=>{
+	if(err) throw err;
+	console.log("connection success!!")
+})*/
 
-conn.end((err)=>{
+/*conn.end((err)=>{
 	if(err) throw err;
 	console.log("connection ended")
-})
+})*/
 
 app.get('/',(req,res)=>{
 	//main page
@@ -54,25 +58,54 @@ app.post('/auth',urlencodedParser,(req,res) => {
 	console.log(req.body.age)
 	res.send(tosend.toString)*/
 })
-
+/*app.all('/adduser',urlencodedParser,(req,res)=>{
+	var add = req.body.adduser;
+	var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress
+	console.log("ip from %s",ip)
+	if(add){
+		//add new user
+		console.log("adduser recieved")
+		console.log("establishing mysql connection")
+		console.log("add user")
+		var email = req.body.email
+		var passwd = req.body.pass
+		var username = req.body.name
+		var sql = 'insert into logininfo (email,username,password) values ("'+ mysql.escapeId(email) + '","' + mysql.escapeId(username) + '","' + mysql.escapeId(passwd) + '");'
+		console.log(sql)
+		conn.query("use tnfsavoting")
+		conn.query(sql)
+		res.send("<script>alert('account created redirecting to login page');location.replace('https:////voting.sivir.pw/login')</script>")
+	}else{
+		//print add user page
+		fs.readFile("static/adduser/index",(err,data)=>{
+			if(err) throw err;
+			console.log("printing adding user page")
+			console.log()
+			res.send(data.toString())
+		})
+	}
+})*/
 app.all('/login',urlencodedParser,(req,res)=>{
 	//the login in app
 	//with the parameter "auth" can lead to different page
 	var auth = req.body.auth
 	var ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress
+	console.log("Requested ip from ip:%s",ip)
 	if(auth){
 		//inside the true auth place
 		var email = req.body.email
 		var passwd = req.body.pass
-		
-		console.log("Logged in from ip:%s",ip)
+		var sql = "select * from logininfo where email="+mysql.escapeId(email)+";"
+		console.log(sql)
+		conn.query(sql,(err,result,fields)=>{
+			console.log(result);
+		})	
 		console.log("Email: %s. passwd: %s",email,passwd)
 		console.log()
 		res.sendStatus(200)
 	}else{
 		//send login website page
 		console.log("printing login page")
-		console.log("requested from ip: %s",ip)
 		console.log()
 		fs.readFile("static/login/index",(err,data)=>{
 			if(err) throw err;
@@ -80,6 +113,10 @@ app.all('/login',urlencodedParser,(req,res)=>{
 		})
 	}
 })
+/*conn.end((err)=>{
+	if(err) throw err;
+	console.log("connection ended")
+})*/
 
 app.listen(port,()=>{
 	console.log("the voting system is running on %d port",port)
