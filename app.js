32ccea7be7e130767c5a655b972e90935273a334
alias722 <maxchengboy@gmail.com>
2021-03-08 10:10:22 +0800

    login

--- app.js
+++ app.js
@@ -19,34 +19,41 @@ const port = 17002 // this node.js is working on this port using nginx proxy to 
 
 //every request will all pass here
 //app.use(cookieParser(cookieParserName),(req,res)
-
+var temp
 app.use(cookieParser(cookieParserName),(req,res,next)=>{
 	//print basic information
 	console.log(Date())
 	var ip = req.headers["x-forwarded-for"] || req.connection.remoteAddress
 	console.log("IP from : %s",ip)
+	var sessionid = req.signedCookies.id
+	console.log("session ID is : %s",sessionid)
 
 	//verify cookie validate
-	var sessionid = req.signedCookies.id
 	var email
 	var username
 	if(sessionid === undefined){
 		//if cookie is not validate
-		console.log("not logged in")
+		console.log("session id not validate")
 		return next()
 	}
 	//check cookie status
-	var temp = 'select * from session where id = "'+mysql.escape(sessionid)+'";'
+	temp = 'select * from session where id = "'+mysql.escape(sessionid)+'";'
+	console.log(temp)
 	conn.query(temp,(err,result,fields)=>{
+		console.log(result)
 		if(result.length == 0){
-			//cookie session not exsist
+			//cookie session not correct
+			res.clearCookie('id',{signed: true,httpOnly: true,overwrite: true})
+			console.log("cookie deleted")
 			return next()
 		}
 		email = result[0].email
 		var time = result[0].time
 		if(Date.now() - time > 1800000){
 			//session is expired
+			res.clearCookie('id',{signed: true,httpOnly: true,overwrite: true})
 			conn.query('delete from session where id = "'+mysql.escape(sessionid)+'";')
+			console.log("cookie expired deleted")
 			return next()
 		}else{
 			//update session
@@ -54,6 +61,7 @@ app.use(cookieParser(cookieParserName),(req,res,next)=>{
 			temp = 'update session set time = '+ timenow + ' where id = "'+mysql.escape(sessionid)+'";'
 			conn.query(temp)
 			res.cookie('id', sessionid, {signed: true, maxAge:1800000,httpOnly: true,overwrite: true});
+			console.log("session time update")
 			return next()
 		}
 	})
@@ -88,7 +96,7 @@ app.get('/',(req,res)=>{
 
 	fs.readFile("static/main/index",(err,data)=>{
 		if(err) throw err
-		res.send(data.toString())
+		return res.send(data.toString())
 	})
 })
 
@@ -105,9 +113,9 @@ app.all('/adduser',urlencodedParser,(req,res)=>{
 			var sql = 'insert into logininfo (email,username,password) values ("'+ mysql.escape(email) + '","' + mysql.escape(username) + '","' + mysql.escape(passwd) + '");'
 			console.log(sql)
 			conn.query(sql)
-			res.send("<script>alert('account created redirecting to login page');location.replace('https://voting.sivir.pw/login')</script>")
+			return res.send("<script>alert('account created redirecting to login page');location.replace('https://voting.sivir.pw/login')</script>")
 		}else{
-			res.send("<script>alert('function not enabled');location.replace('https://voting.sivir.pw/')</script>")
+			return res.send("<script>alert('function not enabled');location.replace('https://voting.sivir.pw/')</script>")
 		}
 	}else{
 		//print add user page
@@ -115,16 +123,16 @@ app.all('/adduser',urlencodedParser,(req,res)=>{
 			if(err) throw err;
 			console.log("printing adding user page")
 			console.log()
-			res.send(data.toString())
+			return res.send(data.toString())
 		})
 	}
 })
 app.all('/auth',urlencodedParser,(req,res)=>{
 	//only verified session can get in
-	var cookieid = req.signedCookies['id']
+	/*var cookieid = req.signedCookies['id']
 	if(cookieid === undefined){
 		//if not logged in return to login page
-		res.send("<script>alert('only verified session can view');location.replace('http://voting.sivir.pw/login')</script>")
+		return res.send("<script>alert('only verified session can view');location.replace('http://voting.sivir.pw/login')</script>")
 	}else{
 		//verify the cookie validate
 		console.log("Cookie id : %s is tring in",cookieid)
@@ -151,35 +159,72 @@ app.all('/auth',urlencodedParser,(req,res)=>{
 				//list student id input form
 			}
 		})
-	}
+	}*/
 	
 })
 
 app.all('/login',urlencodedParser,(req,res)=>{
 	//the login in app
 	//with the parameter "auth" can lead to different page
-	/*if(typeof(req.signedCookies.id)!==undefined){
-		var querymail = "select * from session where id = "+req.signedCookies.id
-		conn.query(querym)
-		var qemail = req.signedCookies.id
-		var sqlque = "update session set time="+Date.now()+" where email="+qemail
-	}*/
 	var sessionid = req.signedCookies.id
-	console.log("session ID is : %s",sessionid)
+			
 	if(!sessionid){
-		var auth = req.body.auth
-		if(auth){
-			//inside the true auth place
-			var email = req.body.email
-			var passwd = req.body.pass
-			var sql = 'select * from logininfo where email="'+mysql.escape(email)+'";'
-			conn.query(sql,(err,result,fields)=>{
-				//var jsonresult = JSON.stringify(result)
-				//var obj = JSON.parse(jsonresult)
-				if(!result.length){
-					console.log("username not match")
-					return res.send("<script>alert('INVALIDE username or password');location.replace('https://voting.sivir.pw/login')</script>")
+		//session not exsist
+		//print login page
+		console.log("printing login page")
+		console.log()
+		fs.readFile("static/login/index",(err,data)=>{
+			if(err) throw err;
+			return res.send(data.toString())
+		})
+	}else{
+		//having session id but not triggered with auth parameter
+		//if verified enter function page
+		//directly print function page
+		
+		//print function page
+		return res.send("Function Page")
+	}
+		//session exsist
+	var auth = req.body.auth
+	if(auth){
+		console.log("entering auth section")
+		//auth exsist verify
+		var email = req.body.email
+		var passwd = req.body.pass
+		temp = 'select * from logininfo where email="'+mysql.escape(email)+'";'
+		console.log(temp)
+		conn.query(temp,(err,result,fields)=>{
+			console.log(result)
+			if(!result.length){
+				//username not queried
+				console.log("username not match")
+				return res.send('<script>alert("INVALID username or password");location.replace("https://voting.sivir.pw/login")</script>')
+			}else{
+				//verify the password
+				var sqlpassword = result[0].password
+				if(sqlpassword == mysql.escape(passwd)){
+					//password is match with the mysql database
+					console.log("Correct")
+					//deploy session id
+					var sessionid = crypto.randomBytes(32).toString('base64')
+					var timenow = Date.now()
+					var sqlquery = 'insert into session (id,time,email) values ("'+mysql.escape(sessionid)+'",'+timenow+',"'+mysql.escape(email)+'");'
+					conn.query(sqlquery)
+					res.cookie('id', sessionid, {signed: true, maxAge:1800000,httpOnly: true,overwrite: true});
+					//reload page
+					console.log("here")
+					//return res.send('<script>location.replace("https://voting.sivir.pw/login")</script>')
+					return res.redirect(302,"https://voting.sivir.pw/login")
 				}else{
+					//password is not match with mysql database
+				}
+			}
+		})
+	}
+	
+
+				/*}else{
 					sqlpassword = result[0].password
 					if(sqlpassword == mysql.escape(passwd)){
 						console.log("correct")
@@ -214,21 +259,29 @@ app.all('/login',urlencodedParser,(req,res)=>{
 		}
 	}else{
 		return res.send("<script>location.replace('https://voting.sivir.pw/function')</script>")
-	}
+	}*/
 })
 app.get("/function",(req,res)=>{
 	//check session id
 	var sessionid = req.signedCookies.id
 	//check sessionid validate
-	var temp = 'select * from session where id = "'+mysql.escape(sessionid)+'";'
-	conn.query(temp,(err,result,fields)=>{
-		if(result.length == 0){
-			//not valid
-			res.cookie('id', sessionid, {signed: true, maxAge:0,httpOnly: true,overwrite: true});
-			return res.send("<script>location.replace('https://voting.sivir.pw/login')</script>")
-		}
-	})	
-	res.sendStatus(200)	
+	if(!sessionid){
+		//no session id
+		return res.send("<script>alert('permission deny');location.replace('https://voting.sivir.pw/login')</script>")
+	}else{
+		//have session id
+		var temp = 'select * from session where id = "'+mysql.escape(sessionid)+'";'
+		conn.query(temp,(err,result,fields)=>{
+			console.log(result)
+			if(result.length == 0){
+				//not valid
+				//res.cookie('id', sessionid, {signed: true, maxAge:0,httpOnly: true,overwrite: true});
+				console.log("Hello")
+				//tobe continued
+			}
+		})
+	}
+
 })
 /*conn.end((err)=>{
 	if(err) throw err;
